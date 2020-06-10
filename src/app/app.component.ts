354aba68e14425cc201861723fbc7693737a649f
Subharaj Mukherjee <subharaj.mukherjee@gmail.com>
2020-06-10 13:12:27 +0530

    second iteration changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'elements2';
+
+  Button1() {
+    localStorage.setItem('demo', 'button1');
+    location.reload();
+  }
+
+  Button2() {
+    localStorage.setItem('demo', 'button2');
+    location.reload();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { createCustomElement} from '@angular/elements';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,15 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private injector: Injector) {
+    const demoEl = createCustomElement(AppComponent, { injector });
+    customElements.define('demo-el', demoEl);
+  }
+
+  ngDoBootstrap() { }
+}
